d76ab63e7deb2facfff66087c0cde946e8dcf9b6
Sanskar <sanskar.mathur@beehyv.com>
2025-01-04 17:58:05 +0530

    Added & Changed existing Shortcuts

--- src/Pages/CanvasPage.tsx
+++ src/Pages/CanvasPage.tsx
@@ -1,20 +1,22 @@
 import { useDispatch } from "react-redux";
+import BasicShapes from "../components/BasicShapeEnum";
 import useKeyboardShortcuts from "../components/useKeyboardShortcuts";
 import { changeTool, redo, undo } from "../redux/paintSlice";
 import Canvas from "./Canvas";
 import ToolBar from "./ToolBar";
-import BasicShapes from "../components/BasicShapeEnum";
 
 const CanvasPage = () => {
 	const dispatch = useDispatch();
 
 	// Tool selection shortcuts
-	useKeyboardShortcuts("l", () => dispatch(changeTool(BasicShapes.Line)));
-	useKeyboardShortcuts("a", () => dispatch(changeTool(BasicShapes.Arrow)));
-	useKeyboardShortcuts("c", () => dispatch(changeTool(BasicShapes.Circle)));
-	useKeyboardShortcuts("r", () => dispatch(changeTool(BasicShapes.Rectangle)));
-	useKeyboardShortcuts("p", () => dispatch(changeTool(BasicShapes.Scribble)));
-	useKeyboardShortcuts("e", () => dispatch(changeTool(BasicShapes.Eraser)));
+	useKeyboardShortcuts("alt+m", () => dispatch(changeTool(BasicShapes.Move)));
+	useKeyboardShortcuts("alt+s", () => dispatch(changeTool(BasicShapes.Selection)));
+	useKeyboardShortcuts("alt+l", () => dispatch(changeTool(BasicShapes.Line)));
+	useKeyboardShortcuts("alt+a", () => dispatch(changeTool(BasicShapes.Arrow)));
+	useKeyboardShortcuts("alt+c", () => dispatch(changeTool(BasicShapes.Circle)));
+	useKeyboardShortcuts("alt+r", () => dispatch(changeTool(BasicShapes.Rectangle)));
+	useKeyboardShortcuts("alt+p", () => dispatch(changeTool(BasicShapes.Scribble)));
+	useKeyboardShortcuts("alt+e", () => dispatch(changeTool(BasicShapes.Eraser)));
 
 	// Undo/Redo shortcuts
 	useKeyboardShortcuts("ctrl+z", () => dispatch(undo()));
