b79d328f16cc4000344f17419c0522a1b561bf9f
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-01-09 18:42:43 +0530

    Type definition

--- src/components/Shape.ts
+++ src/components/Shape.ts
@@ -1,56 +1,41 @@
-import { BasicShapes } from "./ShapeEnum";
+import { AdvancedShapes, BasicShapes } from "./ShapeEnum";
 
 export default interface Shape {
 	id: string; // UUID
-	type: BasicShapes; // Pre-defined types (Arrow, Rectangle, Circle)
-	properties: Record<string, any>; // Custom Properties based on type
+	type: BasicShapes | AdvancedShapes; // Pre-defined types (Arrow, Rectangle, Circle)
+	properties: BasicShapeProps | AdvancedShapesProps; // Custom Properties based on type
 	stroke: string; // Outline color
 	strokeWidth: number; // Outline Width
 	rotation: number; // Angle (in 0-180)
 	fill: string; // Fill color (default - transparent)
 	isDraggable: boolean; // Drag across canvas
+	elements?: ElementProps; // Elements in the shape (Queue, Stack, Array)
 }
 
-/*
-Core shapes are: Rect, Circle, Line, Scribble, Arrow
+export interface BasicShapeProps {
+	x: number;
+	y: number;
+	height?: number;
+	width?: number;
+	radius?: number;
+	points?: number[];
+	startX?: number;
+	startY?: number;
+}
 
-Shape:
-	{
-		id:				UUID
-		type:			String
-		Props:			Object
-		stroke:			String
-		strokeWidth:	Number
-		rotation: 		Number
-		fill:			String
-		isDraggable: 	Boolean
-	}
+export interface AdvancedShapesProps {
+	x: number;
+	y: number;
+	height: number;
+	width: number;
+}
 
-Rectangle :
-	Props: 	{
-				x:		Top-left corner coords
-				y:		Top-left corner coords
-				height:	Bottom-right corner coords
-				width:	Bottom-right corner coords
-			}
-Circle:
-	Props: 	{
-				x:		Center coords
-				y:		Center coords
-				radius:
-			}
-Line:
-	Props: 	{
-				points:	Array(x1,y1,x2,y2)	Starting point and ending point coords
-			}
-Scribble:
-	Props: 	{
-				points:	Array(x1,y1,x2,y2,x3,y3,x4,y4,...)	Coords of all the points on line
-			}
-Arrow:
-	Props:	{
-				points: Array(x1,y1,x2,y2,x3,y3)
-				pointerLength: 
-				pointerWidth: 
-			}
-*/
+export interface ElementProps {
+	values: any[];
+	size: number;
+}
+
+export interface PointerPosition {
+	x: number;
+	y: number;
+}
