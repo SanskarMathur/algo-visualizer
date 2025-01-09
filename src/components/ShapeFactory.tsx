9200931bc987eabd0fe9e9054a31b247e65442e1
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-01-09 18:45:29 +0530

    Advanced Shapes rendering added

--- src/components/ShapeFactory.tsx
+++ src/components/ShapeFactory.tsx
@@ -1,30 +1,42 @@
-import { BasicShapes } from "./ShapeEnum";
 import Shape from "./Shape";
+import { AdvancedShapes, BasicShapes } from "./ShapeEnum";
+import ArrayShape from "./shapes/BasicArray";
 import BasicArrow from "./shapes/BasicArrow";
 import BasicCircle from "./shapes/BasicCircle";
 import BasicEraser from "./shapes/BasicEraser";
 import BasicLine from "./shapes/BasicLine";
+import QueueShape from "./shapes/BasicQueue";
 import BasicRectangle from "./shapes/BasicRectangle";
+import StackShape from "./shapes/BasicStack";
 
 const ShapeFactory = ({ shapes }: { shapes: Shape[] }) => {
 	return (
 		<>
 			{shapes.map((shape) => {
-				switch (shape.type) {
-					case BasicShapes.Rectangle:
-						return <BasicRectangle key={shape.id} {...shape} />;
-					case BasicShapes.Circle:
-						return <BasicCircle key={shape.id} {...shape} />;
-					case BasicShapes.Line:
-					case BasicShapes.Scribble:
-						return <BasicLine key={shape.id} {...shape} />;
-					case BasicShapes.Arrow:
-						return <BasicArrow key={shape.id} {...shape} />;
-					case BasicShapes.Eraser:
-						return <BasicEraser key={shape.id} {...shape} />;
-					default:
-						return null;
-				}
+				if (shape.type in BasicShapes) {
+					switch (shape.type) {
+						case BasicShapes.Rectangle:
+							return <BasicRectangle key={shape.id} {...shape} />;
+						case BasicShapes.Circle:
+							return <BasicCircle key={shape.id} {...shape} />;
+						case BasicShapes.Line:
+						case BasicShapes.Scribble:
+							return <BasicLine key={shape.id} {...shape} />;
+						case BasicShapes.Arrow:
+							return <BasicArrow key={shape.id} {...shape} />;
+						case BasicShapes.Eraser:
+							return <BasicEraser key={shape.id} {...shape} />;
+					}
+				} else if (shape.type in AdvancedShapes) {
+					switch (shape.type) {
+						case AdvancedShapes.Array:
+							return <ArrayShape key={shape.id} {...shape} />;
+						case AdvancedShapes.Queue:
+							return <QueueShape key={shape.id} {...shape} />;
+						case AdvancedShapes.Stack:
+							return <StackShape key={shape.id} {...shape} />;
+					}
+				} else console.error(`Unsupported shape type: ${shape.type}`);
 			})}
 		</>
 	);
